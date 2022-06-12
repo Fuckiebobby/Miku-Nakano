c1721f572d9bd3e7fb85b1bc76d2d597845a81a9
Fuckiebobby <105288063+Fuckiebobby@users.noreply.github.com>
2022-06-12 15:43:54 +0530

    Update script.py

--- MikuXProBot/script.py
+++ MikuXProBot/script.py
@@ -1,24 +1,24 @@
 PM_START_TEXT = """────「 [{}]({}) 」────\n*Hello {}!*\n*I'm An Anime Themed Advance Group Management Bot*\n\n❍ *Uptime:* `{}`\n❍ `{}` *Users, Across* `{}` *Chats*\n\n*Click The Help Button To Get List Of My Commands.*\nThanks To @MikuXProBot For Repo"""
 
 MIKU_IMG = (
-      "https://telegra.ph/file/3f06de01df5bc3c3cf343.jpg",
-      "https://telegra.ph//file/05311caed13176f7d35e5.jpg",
-      "https://telegra.ph//file/88976abda0d0af9d4a517.jpg",
-      "https://telegra.ph//file/b388f473ddfb9cc727bb1.jpg",
-      "https://telegra.ph//file/d950844dad2e473950470.jpg",
+      "https://telegra.ph/file/8b179dd176abda5b63912.jpg",
+      "https://telegra.ph/file/63a33b310fcb281c6eb00.jpg",
+      "https://telegra.ph/file/63a33b310fcb281c6eb00.jpg",
+      "https://telegra.ph/file/8b179dd176abda5b63912.jpg",
+      "https://telegra.ph/file/8b179dd176abda5b63912.jpg",
 )
 
 TEXXT = "<b>Heya</b> {} <b>Do you Like me?</b>"
 
 MIKU_N_IMG = (
-      "https://telegra.ph/file/837c61d9c51236fea4100.jpg",
-      "https://telegra.ph/file/ee34cf0d7e4782424b777.jpg",
-      "https://telegra.ph/file/5410b02359a2cabc2776b.jpg",
-      "https://telegra.ph/file/b1fc3b2af759999bf3b35.jpg",
-      "https://telegra.ph/file/305b5b3c4527cd439b926.jpg"
+      "https://telegra.ph/file/a0346264268665927f17a.jpg",
+      "https://telegra.ph/file/b62e639212e9d255fc816.jpg",
+      "https://telegra.ph/file/3866935aa1b47f9b0f1a1.mp4",
+      "https://telegra.ph/file/b55feb7a1bd7ee9706c2b.jpg",
+      "https://telegra.ph/file/b55feb7a1bd7ee9706c2b.jpg"
 
 )
 
-PM_PHOTO = "https://telegra.ph//file/3a88a958a88e83d85d284.jpg"
+PM_PHOTO = "https://telegra.ph/file/3448596aeb1c19298b62a.jpg"
 
-MIKU_DISPACHER_PIC = "https://telegra.ph//file/4ce99db6136c77248e0ec.jpg"
+MIKU_DISPACHER_PIC = "https://telegra.ph/file/8b179dd176abda5b63912.jpg"
